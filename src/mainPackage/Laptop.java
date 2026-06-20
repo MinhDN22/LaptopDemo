72ecbc70c40a27374275401b9b5fd6c1a2c527ed
MinhDN22.X1C <MinhDN22.X1C@mail>
2026-06-21 00:31:34 +0700

    Update file Laptop.java using Claude Code (for the very 1st time)

--- src/mainPackage/Laptop.java
+++ src/mainPackage/Laptop.java
@@ -2,8 +2,10 @@ package mainPackage;
 
 public class Laptop {
 	public Camera camera;
+	public SoundSpeaker soundSpeaker;
 
 	public Laptop() {
 		this.camera = new Camera();
+		this.soundSpeaker = new SoundSpeaker();
 	}
 }
